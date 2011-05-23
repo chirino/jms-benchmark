fd44209328f164a1218a01ba8c1fef23c0e80e80
Hiram Chirino <hiram@hiramchirino.com>
2011-05-23 11:25:12 -0400

    test against released activemq version.

--- project/build/project.scala
+++ project/build/project.scala
@@ -4,11 +4,11 @@ class BenchmarkProject(info: ProjectInfo) extends DefaultProject(info) {
 
   lazy  val maven_local = "Local Maven Repository" at "file://" + Path.userHome + "/.m2/repository"
 
-  lazy val karaf_console = "org.apache.karaf.shell" % "org.apache.karaf.shell.console" % "2.1.0"
-  lazy val slf4j_nop = "org.slf4j" % "slf4j-nop" % "1.5.11"
-  
-  lazy val activemq = "org.apache.activemq" % "activemq-core" % "5.6-SNAPSHOT"
+  lazy val karaf_console = "org.apache.karaf.shell" % "org.apache.karaf.shell.console" % "2.1.0" intransitive()
+  lazy val activemq = "org.apache.activemq" % "activemq-core" % "5.5.0"
   lazy val stompjms = "org.fusesource.stompjms" % "stompjms-client" % "1.0"
 
+  lazy val slf4j = "org.slf4j" % "slf4j-api" % "1.5.11"
+  lazy val slf4j_nop = "org.slf4j" % "slf4j-nop" % "1.5.11"
 }
 
